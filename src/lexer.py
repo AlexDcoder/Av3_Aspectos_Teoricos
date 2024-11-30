fda3089f6e90e813e0655986e6f1309db2d612ad
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2024-11-30 17:24:19 -0300

    refactor lexer and add parser implementation

--- src/lexer.py
+++ src/lexer.py
@@ -4,13 +4,13 @@ import ply.lex as lex
 reserved = {
     'se': 'SE',
     'enquanto': 'ENQUANTO',
-    'quebra': 'QUEBRA',
+    'quebre': 'QUEBRE',
     'retorne': 'RETORNE',
     'funcao': 'FUNCAO'
 }
 
 tokens = (
-    'VARIAVEL', 'NUMERO', 'MAIS', 'MENOS', 'VEZES', 'DIVIDIDO', 'IGUAL',
+    'VARIAVEL', 'NUMERO', 'MAIS', 'MENOS', 'VEZES', 'DIVIDIDO', 'RECEBE', 'IGUAL',
     'MAIOR', 'MAIOR_IGUAL', 'MENOR', 'MENOR_IGUAL', 'VIRGULA',
     'PARENTESE_D', 'PARENTESE_E', 'CHAVE_D', 'CHAVE_E',
 ) + tuple(reserved.values())
@@ -20,16 +20,17 @@ t_MAIS = r'\+'
 t_MENOS = r'-'
 t_VEZES = r'\*'
 t_DIVIDIDO = r'/'
+t_RECEBE = r'='
 t_IGUAL = r'=='
 t_MAIOR = r'>'
 t_MAIOR_IGUAL = r'>='
 t_MENOR = r'<'
 t_MENOR_IGUAL = r'<='
 t_VIRGULA = r','
-t_PARENTESE_D = r'\('
-t_PARENTESE_E = r'\)'
-t_CHAVE_D = r'\{'
-t_CHAVE_E = r'\}'
+t_PARENTESE_E = r'\('
+t_PARENTESE_D = r'\)'
+t_CHAVE_E = r'\{'
+t_CHAVE_D = r'\}'
 
 # Defining rules for more complex tokens
 
@@ -41,12 +42,12 @@ def t_VARIAVEL(t):
 
 
 def t_FUNCAO(t):
-    r'\w+\( \)'
+    r'\w+\(\)'
     return t
 
 
 def t_NUMERO(t):
-    r'\d+(.\d+)?'
+    r'\d+(\.\d+)?'
     t.value = float(t.value)
     return t
 
@@ -64,6 +65,11 @@ def t_error(t):
 # Ignored characters
 t_ignore = ' \t'
 
+lexer = lex.lex()
+
+def get_lexer():
+    return lexer
+
 if __name__ == '__main__':
     lexer = lex.lex()
 
@@ -73,12 +79,12 @@ funcao minhaFuncao(x, y) {
         retorne x + y
     }
     enquanto (x > 0) {
-        x = x - 1;
-        se (x == 1) quebre;
+        x = x - 1
+        se (x == 1) quebre
     }
     retorne 0
 }
 """
     lexer.input(data)
     for token in lexer:
-        print(token)
+        print(f"{token.type}({token.value}) at line {token.lineno}")
--- src/sintatic.py
+++ src/sintatic.py
@@ -1,1 +1,153 @@
 import ply.yacc as yacc
+from lexer import tokens, get_lexer  # Importa os tokens definidos no lexer
+
+lexer = get_lexer()  
+
+# Nós da árvore sintática abstrata (AST)
+class Node:
+    def __init__(self, node_type, children=None, value=None):
+        self.node_type = node_type
+        self.children = children if children else []
+        self.value = value
+
+    def __repr__(self):
+        return self.to_string()
+
+    def to_string(self, level=0):
+        ret = "\t" * level + f"Node(type={self.node_type}, value={self.value})\n"
+        for child in self.children:
+            # If child is a string, don't call to_string, just append it
+            if isinstance(child, Node):
+                ret += child.to_string(level + 1)
+            else:
+                ret += "\t" * (level + 1) + f"Value: {child}\n"
+        return ret
+
+
+# Regras de produção
+def p_program(p):
+    """program : function"""
+    p[0] = Node("program", [p[1]])
+
+
+def p_function(p):
+    """function : FUNCAO VARIAVEL PARENTESE_E params PARENTESE_D CHAVE_E statements CHAVE_D"""
+    p[0] = Node("function", [p[4], p[7]], p[2])
+
+
+def p_params(p):
+    """params : VARIAVEL
+              | VARIAVEL VIRGULA params
+              | empty"""
+    if len(p) == 2:
+        p[0] = Node("params", value=p[1])
+    elif len(p) == 4:
+        p[0] = Node("params", [Node("param", value=p[1]), p[3]])
+    else:
+        p[0] = Node("params")
+
+
+def p_statements(p):
+    """statements : statement
+                  | statement statements"""
+    if len(p) == 2:
+        p[0] = Node("statements", [p[1]])
+    else:
+        p[0] = Node("statements", [p[1], p[2]])
+
+
+def p_statement(p):
+    """statement : expression
+                 | ENQUANTO PARENTESE_E expression PARENTESE_D CHAVE_E statements CHAVE_D
+                 | SE PARENTESE_E expression PARENTESE_D CHAVE_E statements CHAVE_D
+                 | QUEBRE
+                 | RETORNE expression"""
+    if p[1] == "enquanto":
+        p[0] = Node("while", [p[3], p[6]])
+    elif p[1] == "se":
+        p[0] = Node("if", [p[3], p[6]])
+    elif p[1] == "quebre":
+        p[0] = Node("break")
+    elif p[1] == "retorne":
+        p[0] = Node("return", [p[2]])
+    else:
+        p[0] = p[1]
+        
+        
+def p_statement_assignment(p):
+    """statement : VARIAVEL RECEBE expression"""
+    p[0] = Node("assignment", [p[1], p[3]], "=")
+
+
+def p_expression(p):
+    """expression : term
+                  | expression MAIS term
+                  | expression MENOS term
+                  | comparison"""
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = Node("expression", [p[1], p[3]], p[2])
+
+def p_comparison(p):
+    """comparison : expression MAIOR term
+                  | expression MENOR term
+                  | expression MAIOR_IGUAL term
+                  | expression MENOR_IGUAL term
+                  | expression IGUAL term"""
+    p[0] = Node("comparison", [p[1], p[3]], p[2])
+
+
+def p_term(p):
+    """term : factor
+            | term VEZES factor
+            | term DIVIDIDO factor"""
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = Node("term", [p[1], p[3]], p[2])
+
+
+def p_factor(p):
+    """factor : NUMERO
+              | VARIAVEL
+              | PARENTESE_E expression PARENTESE_D"""
+    if len(p) == 2:
+        p[0] = Node("factor", value=p[1])
+    else:
+        p[0] = p[2]
+
+
+def p_empty(p):
+    """empty :"""
+    p[0] = Node("empty")
+
+
+def p_error(p):
+    if p:
+        print(f"Syntax error at token {p.type} ({p.value}) on line {p.lineno}")
+    else:
+        print("Syntax error at EOF")
+
+
+# Construtor do parser
+parser = yacc.yacc()
+
+if __name__ == "__main__":
+    code = """
+    funcao minhaFuncao(x, y) {
+        se (x < y) {
+            retorne x + y
+        }
+        enquanto (x > 0) {
+            x = x - 1p
+            se (x == 1) 
+                quebre
+            }
+        }
+        retorne 0
+    }
+    """
+    result = parser.parse(code, lexer=lexer)
+    print(result)
+
